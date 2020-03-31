c5fe1571f722ab551b42b1871ea1e24234b9d63c
Vadim Kaushan <admin@disasm.info>
2020-03-31 19:00:43 +0300

    Update the benchmark example

--- avatar-cli/examples/benchmark.rs
+++ avatar-cli/examples/benchmark.rs
@@ -1,5 +1,6 @@
 use avatar_probe_rs::open_probe;
-use std::time::Instant;
+use std::time::{Instant, Duration};
+use std::thread;
 
 fn main() {
     let interface = open_probe();
@@ -15,6 +16,8 @@ fn main() {
     let rps = 1_000_000 / uspr;
     println!("read32:  {} us per request, {} requests per second", uspr, rps);
 
+    thread::sleep(Duration::from_millis(200));
+
     let t0 = Instant::now();
     for i in 0..n {
         let _ = interface.write32(0x2000_0000, i as u32);
@@ -23,4 +26,28 @@ fn main() {
     let uspr = dt.as_micros() / (n as u128);
     let rps = 1_000_000 / uspr;
     println!("write32: {} us per request, {} requests per second", uspr, rps);
+
+    thread::sleep(Duration::from_millis(200));
+
+    let mut buf = [0; 32];
+    let t0 = Instant::now();
+    for _ in 0..n/buf.len() {
+        let _ = interface.read_block32(0x2000_0000, &mut buf);
+    }
+    let dt = t0.elapsed();
+    let uspr = dt.as_micros() / (n as u128);
+    let rps = 1_000_000 / uspr;
+    println!("read_block32:  {} us per request, {} requests per second", uspr, rps);
+
+    thread::sleep(Duration::from_millis(200));
+
+    let buf = [0xdeadbeef; 32];
+    let t0 = Instant::now();
+    for _ in 0..n/buf.len() {
+        let _ = interface.write_block32(0x2000_0000, &buf);
+    }
+    let dt = t0.elapsed();
+    let uspr = dt.as_micros() / (n as u128);
+    let rps = 1_000_000 / uspr;
+    println!("write_block32:  {} us per request, {} requests per second", uspr, rps);
 }
